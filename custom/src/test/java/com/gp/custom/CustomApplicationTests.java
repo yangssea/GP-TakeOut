cfa5a39dfd99cac0872842c4f0c5f6311db1158c
yangsssea <596238662@qq.com>
2020-03-15 16:07:51 +0800

    初始化

---
+++ custom/src/test/java/com/gp/custom/CustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gp.custom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
